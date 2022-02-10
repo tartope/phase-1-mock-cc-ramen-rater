ae98643d62be06907c269fdf6b45c4fba1046274
Tunisia Artope <artope@yahoo.com>
2022-02-10 11:05:01 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // write your code here
+//See all ramen images in the div with the id of ramen-menu. 
+//When the page loads, request the data from the server to get all the ramen objects. 
+//Then, display the image for each of the ramen using an img tag inside the #ramen-menu div.
+
+function getRestaurants() {
+    return fetch('http://localhost:3000/ramens')
+        .then(response => response.json())
+        .then(data => (data));
+}
+// console.log(getRestaurants())
+
+function buildImages(){
+    const pictures = document.getElementById('ramen-menu');
+    pictures.appendChild('img').textContent('ramens');
+    
+}
